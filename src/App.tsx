832d5f935077f9022bef573267b8399d52286c37
wallisonkauydev <dev5@hyerdev.com>
2024-08-06 12:38:43 -0300

    feat: Atualizar título da página para "Hyerdev Desafio"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import hyerdevLogo from "/logo-dark.svg";
 
 function App() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-t from-[#150d21] to-[#180f27] w-full flex items-center justify-center">
+      {" "}
+      <img src={hyerdevLogo} alt="Hyerdev Logo" className="w-[600px]" />
+    </div>
+  );
 }
 
 export default App;
